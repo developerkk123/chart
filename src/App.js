4c573e51a7f4532b718e5c4006e3b81a32b7cc49
kundan <kundan505020@gmail.com>
2020-06-01 20:38:32 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Bitgraph from './Dashboard/Graph/BitdevGraph/Bitgraph';
+
+function App() {
+  return (
+    <div className="App">
+      <Bitgraph />
+    </div>
+  );
+}
+
+export default App;
+
+
